6eadd6634564979599b1652b91c395d9807b6af1
izlatkin <zlatkin_ilya@mail.ru>
2021-08-30 21:02:43 -0400

    Initial Commit

---
+++ movie_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie_app
+//
+//  Created by Ilya Zlatkin on 30.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
